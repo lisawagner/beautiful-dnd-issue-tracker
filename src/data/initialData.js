91a491f11d145d8fd4d602d54810317dfd11612f
Lisa Wagner <lisawagner.online@gmail.com>
2022-08-29 14:55:06 -0700

    chore: create sample data

---
+++ src/data/initialData.js
@@ -0,0 +1,17 @@
+const dataset = {
+  tasks: {
+      "task-1": { id: "task-1", content: "Test" },
+      "task-2": { id: "task-2", content: "Build Authentication" },
+      "task-3": { id: "task-3", content: "Add Navigation" },
+      "task-4": { id: "task-4", content: "Create Landing Page" }
+  },
+  columns: {
+      "column-1": { id: "column-1", title: "Issues", taskIds: ['task-1'] },
+      "column-2": { id: "column-2", title: "In progress", taskIds: ['task-2', 'task-3'] },
+      "column-3": { id: "column-3", title: "Review", taskIds: [] },
+      "column-4": { id: "column-4", title: "Completed", taskIds: ["task-4"] }
+  },
+  columnOrder: ["column-1", "column-2", "column-3", "column-4"]
+}
+
+export default dataset
